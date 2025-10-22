e52a81dc8ce8f81797a2b7ba73034b1a62883a4a
Ceres Barros <ceres.barros@nrcan-rncan.gc.ca>
2025-10-22 10:06:51 -0700

    reexport objs from reproducible and gpboost

---
+++ R/reexports.R
@@ -0,0 +1,9 @@
+## non-exported imports from other packages -------------------------------------
+getDrv <- getFromNamespace("getDrv", "reproducible")
+gpb.GPModel <- getFromNamespace("gpb.GPModel", "gpboost")
+
+## re-exported functions --------------------------------------------------------
+## (example not used here)
+## @importFrom reproducible paddedFloatToChar
+## @export
+## reproducible::paddedFloatToCha
